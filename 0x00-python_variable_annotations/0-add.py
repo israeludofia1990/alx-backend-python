64e02676c0fa7e8f0a4c7a7082d80d03148c1dba
udofiaisrael1990 <udofiaisrael170@gmail.com>
2024-04-18 16:14:03 +0000

    first commit

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+'''Python type annotation'''
+
+
+def add(a: float, b: float) -> float:
+    '''type annotation function that returns sum of a and b'''
+    return a + b
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+    Truncate float to integer
+"""
+
+def floor(n: float) -> int:
+    ''' takes a float n as argument and returns the floor of the float.'''
+    return int(n)
