17a579b3e6e9d860f9fd496c1cbf821e7eaf4b25
israeludofia1990 <udofiaisrael170@gmail.com>
2024-06-07 13:32:28 +0100

    properly anotated lst as a list of iterable

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 '''Let's duck type an iterable object'''
-from typing import List, Tuple
+from typing import List, Tuple, Sequence, Iterable
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
     Takes a list of strings and returns a list of
     tuples with each string and its length.
