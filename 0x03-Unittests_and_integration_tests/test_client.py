8fae777ceae3451214e8d3704e4475994e45e14e
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-05 12:21:10 +0100

    documented  test_access_nested_map_0 method

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+'''Test Github client module'''
+import unittest
+from typing import Dict
+from unittest.mock import (
+    patch,
+    MagicMock,
+    Mock,
+    PropertyMock,
+)
+from parameterized import parameterized
+from client import GithubOrgClient
+
+
+class TestGithubOrgClient(unittest.TestCase):
+    """Tests the `GithubOrgClient` class."""
+    @parameterized.expand([
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
+    ])
+    @patch(
+        "client.get_json",
+    )
+    def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
+        """Tests the `org` method."""
+        mocked_fxn.return_value = MagicMock(return_value=resp)
+        gh_org_client = GithubOrgClient(org)
+        self.assertEqual(gh_org_client.org(), resp)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
+        )
+
+    def test_public_repos_url(self) -> None:
+        """Tests the `_public_repos_url` property."""
+        with patch(
+                "client.GithubOrgClient.org",
+                new_callable=PropertyMock,
+                ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/users/google/repos",
+            }
+            self.assertEqual(
+                GithubOrgClient("google")._public_repos_url,
+                "https://api.github.com/users/google/repos",
+            )
+
+
+if __name__ == "__main__":
+    unittest.main()
