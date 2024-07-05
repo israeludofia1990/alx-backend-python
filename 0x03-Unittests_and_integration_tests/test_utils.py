51085092605298f39488faf2b5905dde9bf0b8e4
israeludofia1990 <udofiaisrael170@gmail.com>
2024-07-05 07:59:17 +0100

    documented  test_access_nested_map_0 method

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,6 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
     def test_access_nested_map(self, nested_map, path, expected):
+        '''Test method for the access_nested_map function.'''
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
